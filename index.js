654b9620647d041325ba9fb0ba87c9591bcfc3aa
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2022-06-28 12:28:34 +0200

    building file path and extension of file automatically

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
-    if(req.url == '/'){
+    /* if(req.url == '/'){
         fs.readFile(path.join(__dirname, 'public', 'index.html'), (err,content)=>{
             if(err) throw err;
             res.writeHead(200, {'Content-Type': 'text/html'});
@@ -25,7 +25,37 @@ const server = http.createServer((req,res)=>{
         ];
         res.writeHead(200, {'Content-Type': 'application/json'});
         res.end(JSON.stringify(users));
-    }
+    } */
+
+    // Build file path
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html': req.url );
+    console.log(filePath);
+    res.end();
+
+    // Extension of file
+    let extname = path.extname(filePath);
+
+    // Initial content type
+    let contentType = 'text/html';
+
+    //Check ext and set content type
+    switch (extname) {
+        case ".js":
+          contentType = "text/javascript";
+          break;
+        case ".css":
+          contentType = "text/css";
+          break;
+        case ".json":
+          contentType = "application/json";
+          break;
+        case ".png":
+          contentType = "image/png";
+          break;
+        case ".jpg":
+          contentType = "image/jpg";
+          break;
+      }
 });
 
 const PORT = process.env.PORT || 5000;
