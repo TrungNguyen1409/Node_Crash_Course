563c8f9092a301add3f2aff5c4fd46f189d52cd2
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2022-06-28 11:53:36 +0200

    move code between files

--- logger.js
+++ logger.js
@@ -13,4 +13,27 @@ class Logger extends EventEmitter{
     }
 }
 
-module.exports = Logger;
+//Module Wrapper Function
+
+//console.log(__dirname, __filename);
+
+const Person = require('./person'); // common JS khac voi ES6
+
+
+const person1 = new Person('John Doe', 30);
+
+person1.greeting();
+
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log(`Called Listener: ` ,data));
+
+logger.log('Hello World');
+logger.log('Hello World');
+
+logger.log('hey there');
+
+// idea: log id and msg to a file and then have a file where all the log is stored using appendfile()
