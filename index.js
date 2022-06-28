227e05c1f0b8d12b82c95b5d2d7ae1feb289d888
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2022-06-28 12:18:59 +0200

    load html files using fs.readFile()

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
     if(req.url == '/'){
-        res.end('<h1> Home <h1>');
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err,content)=>{
+            if(err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content);
+        });
+    }
+    if(req.url == '/about'){
+        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err,content)=>{
+            if(err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content);
+        });
     }
 });
 
